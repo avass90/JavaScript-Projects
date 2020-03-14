059e05f244f4213b8304c75b60c9b95dd9ee0b5d
avass90 <60933123+avass90@users.noreply.github.com>
2020-03-13 20:12:26 -0600

    Push Basic JavaScript Folder
    Step 63

---
+++ Basic JavaScript Projects/JS/Basic_JavaScript_2.js
@@ -0,0 +1,11 @@
+function Step_60_Function() {
+    var fri="I love Fridays!";
+    var result=fri.fontcolor("red");
+    document.getElementById("red_Friday") .innerHTML= result;
+}
+
+function step61Function() {
+    var sentence= "I am executing";
+    sentence += " step 61 of the JavaScript Course!";
+    document.getElementById("step61") .innerHTML=sentence;
+}
